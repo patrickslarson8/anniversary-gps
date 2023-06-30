63bc48a6a89422940559ca91a81cf63fc7cbada2
Patrick Larson <pat.s.larson8@gmail.com>
2023-06-29 19:16:13 -0700

    Move display test .ino to seperate folder

--- Utilities/arduino-gps-display.ino
+++ Utilities/arduino-gps-display.ino
@@ -12,15 +12,20 @@ Adafruit_GPS GPS(&mySerial);
 uint32_t timer = millis();
 
 // Display settings
+// Initialize display class
+// args (display type, CS pin,# displays connected)
 MD_Parola P = MD_Parola(MD_MAX72XX::FC16_HW, 10, 4);
-// #define SPEED_TIME  25
-// #define PAUSE_TIME  1000
+#define SPEED_TIME  25
+#define PAUSE_TIME  0
+#define DISPLAY_EFFECT PA_SCROLL_LEFT
+#define DISPLAY_ALIGN PA_LEFT
 
 
 // Game settings
-const char msg_no_gps_msg[7] = { "No GPS" };
-const char msg_gps_fix[4] = { "Fix" };
-
+// messages must end with NUL character or MD Parola overruns
+const char msg_no_gps_msg[8] = { "No GPS\0" };
+const char msg_gps_fix[5] = { "Fix\0" };
+const char msg_boot[6] = { "Boot\0" };
 
 void setup()
 {
@@ -32,20 +37,27 @@ void setup()
 
   delay(1000);
 
-  // Ask for firmware version
+  // Ask for GPS firmware version
   GPS.println(PMTK_Q_RELEASE);
 
   // Setup Displays
   P.begin();
+  P.setTextBuffer(msg_boot);
+  P.setTextAlignment(DISPLAY_ALIGN);
+  P.setSpeed(SPEED_TIME);
+  P.setPause(PAUSE_TIME);
+  P.setTextEffect(DISPLAY_EFFECT, DISPLAY_EFFECT);
+  P.displayReset();
 }
 
 void loop()
 {
+  // testing code for display and GPS integration
+  // read data from the GPS in the 'main loop'
+  //char c = GPS.read();
 
-    // testing code for display and GPS integration
-
-// read data from the GPS in the 'main loop'
-  char c = GPS.read();
+  // call display animate as often as practicable IAW library docs
+  P.displayAnimate();
 
   // if a sentence is received, we can check the checksum, parse it...
   if (GPS.newNMEAreceived()) {
@@ -60,12 +72,12 @@ void loop()
   if (millis() - timer > 2000) {
     timer = millis(); // reset the timer
     if (GPS.fix) {
-      P.displayText(msg_gps_fix, PA_LEFT, 25, 2000, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
-      while(!P.displayAnimate());
+      P.displayText(msg_gps_fix, PA_LEFT, SPEED_TIME, PAUSE_TIME, DISPLAY_EFFECT, DISPLAY_EFFECT);
+
     }
     else {
-      P.displayText(msg_no_gps_msg, PA_LEFT, 25, 2000, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
-      while(!P.displayAnimate());
+      P.displayText(msg_no_gps_msg, PA_LEFT, SPEED_TIME, PAUSE_TIME, DISPLAY_EFFECT, DISPLAY_EFFECT);
+
     }
   }
 }
